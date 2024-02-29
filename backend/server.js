ca04d9733868657c59ad1cbf23e43932be5af046
tyleryannes94 <tyler.yannes@gmail.com>
2024-02-28 22:14:20 -0600

    finished server.js file

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const routes = require('./routes/index'); // Adjust the path as per your new structure
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const store = new MongoDBStore({
+    uri: process.env.DATABASE_URL,
+    collection: 'sessions',
+  });
+  
+  store.on('error', function(error) {
+    console.error('Session store error:', error);
+  });
+
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } 
+  }));
+
+  app.use(cors({
+    origin: '*', 
+    credentials: true, 
+  }));
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.use('/', routes);
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });  
+
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB database'))
+  .catch(err => console.error('MongoDB connection error:', err));
+  
+  // Start the server
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
