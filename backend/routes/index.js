03a3e30252bbe2cdcdc054dbf3974e46702dccc6
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-01 08:15:35 -0600

    Updated connect-mongodb-session to connect-mongo and updated the package json with the latest versions

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const apiRoutes = require('../controllers/api'); 
-const homeRoutes = require('../routes/homeRoutes');
+const homeRoutes = require('./homeRoutes');
 
 const router = express.Router();
 
--- backend/server.js
+++ backend/server.js
@@ -3,52 +3,49 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const mongoose = require('mongoose');
-// const MongoDBStore = require('connect-mongodb-session')(session);
-const routes = require('./routes/index'); // Adjust the path as per your new structure
+const MongoStore = require('connect-mongo'); 
+const routes = require('./routes/index');
 const path = require('path');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const store = new MongoDBStore({
-    uri: process.env.DATABASE_URL,
-    collection: 'sessions',
-  });
-  
-  store.on('error', function(error) {
-    console.error('Session store error:', error);
-  });
+mongoose.connect(process.env.DATABASE_URL)
+.then(() => console.log('Connected to MongoDB database'))
+.catch(err => console.error('MongoDB connection error:', err));
 
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    store: store,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } 
-  }));
+const sessionStore = MongoStore.create({
+  mongoUrl: process.env.DATABASE_URL,
+  collectionName: 'sessions'
+});
 
-  app.use(cors({
-    origin: '*', 
-    credentials: true, 
-  }));
+sessionStore.on('error', function(error) {
+  console.error('Session store error:', error);
+});
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } 
+}));
 
-  app.use('/', routes);
+app.use(cors({
+  origin: '*', 
+  credentials: true, 
+}));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-  });  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to MongoDB database'))
-  .catch(err => console.error('MongoDB connection error:', err));
-  
-  // Start the server
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use('/', routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});  
+
+// Start the server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
