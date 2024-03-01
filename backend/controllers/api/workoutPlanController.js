451d405f94ec08864b61e6e1ac906f076c1fe206
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-01 08:40:33 -0600

    removed sessions and mongodb sessions

--- backend/controllers/api/workoutPlanController.js
+++ backend/controllers/api/workoutPlanController.js
@@ -1,6 +1,6 @@
-const WorkoutPlan = require('../../models/WorkoutPlan'); // Adjust the path as necessary
-const User = require('../../models/User');
-const Workout = require('../../models/Workout'); // Adjust the path as necessary
+const WorkoutPlan = require('../../../backend/models/WorkoutPlan'); // Adjust the path as necessary
+const User = require('../../../backend/models/User');
+const Workout = require('../../../backend/models/Workout'); // Adjust the path as necessary
 
 exports.getAllWorkoutPlansForUser = async (req, res) => {
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const session = require('express-session');
 const mongoose = require('mongoose');
-const MongoStore = require('connect-mongo'); 
 const routes = require('./routes/index');
 const path = require('path');
 const cors = require('cors');
@@ -11,30 +9,14 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Connect to MongoDB
 mongoose.connect(process.env.DATABASE_URL)
 .then(() => console.log('Connected to MongoDB database'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-const sessionStore = MongoStore.create({
-  mongoUrl: process.env.DATABASE_URL,
-  collectionName: 'sessions'
-});
-
-sessionStore.on('error', function(error) {
-  console.error('Session store error:', error);
-});
-
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  store: sessionStore,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } 
-}));
-
 app.use(cors({
-  origin: '*', 
-  credentials: true, 
+    origin: '*', 
+    credentials: true, 
 }));
 
 app.use(express.json());
@@ -44,8 +26,8 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.use('/', routes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});  
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 // Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
