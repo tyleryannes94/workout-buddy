6d014eaf00b0d87a491a4cedbc0aab4c140e2ea2
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-02 02:12:12 -0600

    re-added the user routes files I deleted

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const userController = require('../controllers/api/userController');
+const router = express.Router();
+
+router.post('/signup', userController.createUser);
+router.post('/login', userController.loginUser);
+router.get('/:id', userController.getUserById);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
+module.exports = router;
