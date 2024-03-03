3a4d4eb26fb74d4d38cff902aedd9b983d8c5458
Suzet Salinas <suzet.sal@gmail.com>
2024-03-03 14:58:27 -0600

    commented out some divs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,22 +8,22 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Picante Programmers</h1>
+      </div> */}
+      <h1>Workout Buddy by Picante Programmers</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p>
+        </button> */}
+        {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
