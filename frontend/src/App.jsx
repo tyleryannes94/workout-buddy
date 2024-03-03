0b8da0a68734cf8d4d0cfc95954c09c06ce90cad
Suzet Salinas <suzet.sal@gmail.com>
2024-03-03 15:40:55 -0600

    several files were (seemingly incorrectly) using 'react-router-dom' rather than the installed dependency 'react-dom'.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-dom';
 import Login from './components/Login';
 import SignupForm from './components/SignUpForm';
 import Dashboard from './components/Dashboard';
