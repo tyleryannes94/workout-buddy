184d43877d80c5b444e4146070b915b02b1261d1
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-03 19:19:35 -0600

    reverted to react-router-dom

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-dom'; 
+import { useNavigate } from 'react-router-dom'; 
 
 const Login = () => {
     const [email, setEmail] = useState('');
--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-dom'; 
+import { useNavigate } from 'react-router-dom'; 
 
 function SignupForm() {
   const [formData, setFormData] = useState({
