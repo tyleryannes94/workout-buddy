8383b8050d313d22b5158cc560d858005451f9f3
Suzet Salinas <suzet.sal@gmail.com>
2024-03-06 17:13:18 -0600

    added a Home component and rerouted App to go thru Home, then Login. also centered body

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,22 +6,27 @@ import Dashboard from './components/Dashboard';
 import CreateMeals from './components/CreateMeals';
 import CreateWorkouts from './components/CreateWorkouts';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 
 function App() {
   return (
+    <>
     <Router>
-      <Navbar />
-      <Login />
+      {/* <Navbar /> */}
       <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignupForm />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/meals" element={<CreateMeals />} />
-      <Route path="/workouts" element={<CreateWorkouts />} />
 
+        <Route path='/' exact element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/meals" element={<CreateMeals />} />
+        <Route path="/workouts" element={<CreateWorkouts />} />
+        
       </Routes>
     </Router>
+    </>
+    
   )
 }
 
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App'; 
+// import '../index.css'; already imported in main.jsx
+import Login from "./Login";
+
+function Home() {
+    return (
+        <>
+            <Login />
+        </>
+    );
+}
+
+export default Home;
