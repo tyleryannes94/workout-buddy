876a3e35fae7fc82007003bc1de47e752afb6dde
Donnie1129 <drn112995@gmail.com>
2024-03-06 20:23:34 -0600

    added file for the Analytics.js code

---
+++ backend/models/Analytics.js
@@ -0,0 +1,13 @@
+// Must have
+
+// total meals logged - 
+// calorie intake - 
+// workouts completed - 
+// calorie burned - 
+
+
+// Extras
+
+// count per workout type - 
+// count per meal type - 
+// calories per meal type - 
