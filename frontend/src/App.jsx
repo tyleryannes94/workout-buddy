506e1786c955b6d2724ed0da69cdc57d1d42fc5b
Donnie1129 <drn112995@gmail.com>
2024-03-11 19:30:07 -0500

    Created the routes and styling for the Analytics page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,25 +7,23 @@ import CreateMeals from './components/CreateMeals';
 import CreateWorkouts from './components/CreateWorkouts';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-
+import Analytics from './components/Analytics'; // Import Analytics component
 
 function App() {
   return (
-    <>
     <Router>
       <Routes>
-      <Route path='/' exact element={<Home />} />
-      <Route path="/navbar" element={<Navbar />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignupForm />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/meals" element={<CreateMeals />} />
-      <Route path="/workouts" element={<CreateWorkouts />} />
+        <Route path='/' exact element={<Home />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/meals" element={<CreateMeals />} />
+        <Route path="/workouts" element={<CreateWorkouts />} />
+        <Route path="/analytics" element={<Analytics />} /> {/* Add route for Analytics */}
       </Routes>
     </Router>
-    </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
