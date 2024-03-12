53c663932f58b81a682952dec5d91dbecfbfc7e3
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-11 22:36:34 -0500

    simplified the progress bar to just show total out of a static 30 workouts

--- frontend/src/components/Friend.jsx
+++ frontend/src/components/Friend.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
 
 function Friend() {
     const [searchTerm, setSearchTerm] = useState('');
@@ -77,6 +78,7 @@ const acceptFriendRequest = async (requestId) => {
 
     return (
         <div>
+        <Navbar/>
             <input
                 type="text"
                 placeholder="Search users by name or email"
