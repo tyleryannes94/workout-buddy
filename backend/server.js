d9682739989b81df833c1ab8a961644162aa8731
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-13 01:31:06 -0500

    added use cors to server

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,3 @@
-// require('dotenv').config();
-
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const routes = require('./routes/index');
-// const path = require('path');
-// const cors = require('cors');
-// const { graphqlHTTP } = require('graphql-http');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// // Connect to MongoDB
-// mongoose.connect(process.env.DATABASE_URL)
-// .then(() => console.log('Connected to MongoDB database'))
-// .catch(err => console.error('MongoDB connection error:', err));
-
-// app.use(cors({
-//     origin: '*', 
-//     credentials: true, 
-// }));
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-// app.use('/', routes);
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-// });
-
-// // Start the server
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 require('dotenv').config();
 const express = require('express');
@@ -46,6 +12,8 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(cors()); 
 
 const server = new ApolloServer({
   typeDefs,
