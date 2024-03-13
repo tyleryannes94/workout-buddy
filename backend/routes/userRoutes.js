6840a6bcebc3cb2f114260dd7cc48aea43bcd93a
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-13 02:22:06 -0500

    another try

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const userController = require('../controllers/api/userController');
-const authenticateToken = require('../utils/auth');
+// const authenticateToken = require('../utils/auth');
 const router = express.Router();
 
 router.post('/signup', userController.createUser);
 router.post('/login', userController.loginUser);
-router.get('/:id', authenticateToken, userController.getUserById);
-router.put('/:id', authenticateToken, userController.updateUser);
+router.get('/:id', userController.getUserById);
+router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
