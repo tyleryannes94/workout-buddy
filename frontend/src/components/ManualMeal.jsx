d9310ccee520cf3f81515ed5caa9bb1cbf760635
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-13 00:00:04 -0500

    updated manual workout/meal component, fixed login comps signup link, and added a new className

--- frontend/src/components/ManualMeal.jsx
+++ frontend/src/components/ManualMeal.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 
 function ManualMeal() {
   const [mealData, setMealData] = useState({
+    date_logged: '',
     mealType: '',
-    description: '',
     calories: '',
-    ingredients: [],
   });
+  const [showForm, setShowForm] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -15,7 +15,7 @@ function ManualMeal() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const userId = localStorage.getItem('userId');; 
+    const userId = localStorage.getItem('userId');
     try {
         const response = await fetch(`/api/meals/meal/${userId}`, {
           method: 'POST',
@@ -23,35 +23,57 @@ function ManualMeal() {
           body: JSON.stringify(mealData), 
         });
         if (!response.ok) throw new Error('Failed to log a new meal');
-        console.log("Meal submitted successfully");
+        
+        alert("Meal logged successfully"); 
+        setShowForm(false);
+        setMealData({ date_logged: '', mealType: '', calories: '' });
       } catch (error) {
-        console.error('Error generating new meal plan:', error);
-        // Update your state or UI based on the error
+        console.error('Error logging new meal:', error);
       }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        name="date_logged"
-        value={mealData.date_logged}
-        onChange={handleChange}
-        placeholder="Date"
-      />
-    <input
-        name="mealType"
-        value={mealData.mealType}
-        onChange={handleChange}
-        placeholder="Meal Type"
-    /> 
-     <input
-        name="calories"
-        value={mealData.calories}
-        onChange={handleChange}
-        placeholder="Total Calories"
-    />     
-    <button type="submit">Submit Meal</button>
-    </form>
+    <div>
+      {showForm ? (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Date:
+            <input
+              type="date" 
+              name="date_logged"
+              value={mealData.date_logged}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Meal Type:
+            <select 
+              name="mealType"
+              value={mealData.mealType}
+              onChange={handleChange}>
+              <option value="">Select Meal Type</option>
+              <option value="breakfast">Breakfast</option>
+              <option value="lunch">Lunch</option>
+              <option value="dinner">Dinner</option>
+              <option value="snack">Snack</option>
+            </select>
+          </label>
+          <label>
+            Calories:
+            <input
+              type="number" 
+              name="calories"
+              value={mealData.calories}
+              onChange={handleChange}
+              placeholder="Total Calories"
+            />
+          </label>
+          <button type="submit">Submit Meal</button>
+        </form>
+      ) : (
+        <button onClick={() => setShowForm(true)}>Add Meal</button> 
+      )}
+    </div>
   );
 }
 
--- frontend/src/components/ManualWorkout.jsx
+++ frontend/src/components/ManualWorkout.jsx
@@ -2,21 +2,31 @@ import React, { useState } from 'react';
 
 function ManualWorkout() {
   const [workoutData, setWorkoutData] = useState({
+    workout_date: '',
     workout_type: '',
-    workout_description: '',
     calories_burned: '',
-    workout_info: [],
+    workout_info: [''],
   });
+  const [showForm, setShowForm] = useState(false);
 
-  const handleChange = (e) => {
+  const handleChange = (e, index) => {
     const { name, value } = e.target;
-    setWorkoutData({ ...workoutData, [name]: value });
+    if (name === "workout_info") {
+      const updatedInfo = [...workoutData.workout_info];
+      updatedInfo[index] = value;
+      setWorkoutData({ ...workoutData, workout_info: updatedInfo });
+    } else {
+      setWorkoutData({ ...workoutData, [name]: value });
+    }
   };
 
+  const addWorkoutInfo = () => {
+    setWorkoutData({ ...workoutData, workout_info: [...workoutData.workout_info, ''] });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const userId = localStorage.getItem('userId');; 
+    const userId = localStorage.getItem('userId');
     try {
       const response = await fetch(`/api/workouts/workout/${userId}`, {
         method: 'POST',
@@ -24,40 +34,63 @@ function ManualWorkout() {
         body: JSON.stringify(workoutData),
       });
       if (!response.ok) throw new Error('Failed to log a manual workout');
-      console.log("Workout successfully logged");
+      alert("Workout successfully logged");
+      setShowForm(false); 
     } catch (error) {
       console.error('Error logging a workout:', error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        name="date_created"
-        value={workoutData.date_created}
-        onChange={handleChange}
-        placeholder="Date"
-      />
-       <input
-        name="workout_type"
-        value={workoutData.workout_type}
-        onChange={handleChange}
-        placeholder="Workout Type"
-      />
-       <input
-        name="workout_info"
-        value={workoutData.info}
-        onChange={handleChange}
-        placeholder="Exercise(s)"
-      />
-       <input
-        name="calories_burned"
-        value={workoutData.calories_burned}
-        onChange={handleChange}
-        placeholder="Calories Burned"
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      {showForm ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="date"
+            name="workout_date"
+            value={workoutData.workout_date}
+            onChange={handleChange}
+            placeholder="Workout Date"
+          />
+          <select
+            name="workout_type"
+            value={workoutData.workout_type}
+            onChange={handleChange}
+          >
+            <option value="">Select Workout Type</option>
+            <option value="Weightlifting">Weightlifting</option>
+            <option value="Yoga">Yoga</option>
+            <option value="Running">Running</option>
+            <option value="Spin">Spin</option>
+            <option value="Boxing">Boxing</option>
+            <option value="Sauna">Sauna</option>
+            <option value="Swimming">Swimming</option>
+            <option value="HIIT">HIIT</option>
+          </select>
+          {workoutData.workout_info.map((info, index) => (
+            <input
+              key={index}
+              type="text"
+              name="workout_info"
+              value={info}
+              onChange={(e) => handleChange(e, index)}
+              placeholder="Exercise(s)"
+            />
+          ))}
+          <button type="button" onClick={addWorkoutInfo}>+</button>
+          <input
+            type="number"
+            name="calories_burned"
+            value={workoutData.calories_burned}
+            onChange={handleChange}
+            placeholder="Calories Burned"
+          />
+          <button type="submit">Submit Workout</button>
+        </form>
+      ) : (
+        <button onClick={() => setShowForm(true)}>Add Workout</button>
+      )}
+    </div>
   );
 }
 
