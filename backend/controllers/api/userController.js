cff91adc5f23957cbac8cc5788f837664fb10118
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-13 07:10:50 -0500

    last try

--- backend/controllers/api/userController.js
+++ backend/controllers/api/userController.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-const jwt = require('../../utils/auth');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
 
 const JWT_SECRET = process.env.JWT_SECRET;
