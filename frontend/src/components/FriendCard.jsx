a3ad04737d4a0aa2a7985a82ade53c11bc518f45
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-13 00:49:35 -0500

    fixed the friends component

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -25,22 +25,27 @@ const FriendCard = ({ searchTerm, users, pendingRequests, workouts, searchUsers,
             </div>
         ))}
 
-        <h2>Pending Requests</h2>
-        {pendingRequests.map(request => (
+        {pendingRequests.length > 0 && (
+        <>
+            <h2>Pending Requests</h2>
+            {pendingRequests.map(request => (
             <div key={request._id}>
                 From: {request.requester.email}
                 <button onClick={() => acceptFriendRequest(request._id)}>Accept</button>
             </div>
-        ))}
+            ))}
+        </>
+        )}
 
         <h2>Friend Workouts</h2>
+        
         {workouts
         .filter(workout => workout.workout_date != null) 
         .map((workout, index) => (
-            <div key={index}>
-                <h3>{workout.userId.first_name}'s Logged Workouts</h3>
+            <div key={index} className="border-accent">
+                <h3 >{workout.userId.first_name}'s Logged Workouts</h3>
                 <p>Type: {workout.workout_type}</p>
-                <p>Date: {workout.workout_date}</p>
+                <p>Date: {new Date(workout.workout_date).toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' })}</p>
                 <p>Calories Burned: {workout.calories_burned}</p>
             </div>
         ))}
