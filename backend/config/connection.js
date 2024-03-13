cda1fbd91d648d639ef7d6910b28450d7fd6a6bb
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-13 04:24:06 -0500

    changed atlas url

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/workout-buddy', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/workout-buddy', {
+
 })
 
 module.exports = mongoose.connection;
