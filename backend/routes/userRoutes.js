3ca0fdea56edc94228efe756a5db0dd9ee761566
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-13 06:59:00 -0500

    differentiated user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.post('/signup', userController.createUser);
 router.post('/login', userController.loginUser);
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get('/getuser/:id', userController.getUserById);
+router.put('/updateuser/:id', userController.updateUser);
+router.delete('/deleteuser/:id', userController.deleteUser);
 
 module.exports = router;
